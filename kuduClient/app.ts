d1ad4c713af6f7107ae32e6a16367ed3f57ac538
Martin Wåger <martin.w@comaround.se>
2016-01-24 16:53:12 +0100

    base

---
+++ kuduClient/app.ts
@@ -0,0 +1,9 @@
+﻿var request = require('request');
+
+request
+
+request('https://livewebjob.scm.azurewebsites.net/azurejobs/api/functions/definitions/azure-webjobs-host-87072d836e694ed089c6ccf16e071622_Zero.WebJob.Single.Functions.SolrReindex/invocations?limit=100', (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        console.log(body); // Print the google web page.
+    }
+})
